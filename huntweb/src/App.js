3b5fe05c7702b3539ac776e3dd61fd09203895ba
4008924 <alex.gomes@riachuelo.com.br>
2020-06-22 17:07:39 -0300

    Adding projeto react-js para aprendizadp

---
+++ huntweb/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+import Header from './components/Header';
+
+const App  =() => (
+  <div className="App">
+    <Header /> 
+  </div>
+);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div>Hello Rocketseat</div>
+//     </div>
+//   );
+// }
+
+export default App;
